74e5ba2e1d7bc45b03429f99c6041f4e9477ba6f
Abhay Walia <abhaywalia2002@gmail.com>
2023-09-13 17:43:37 -0400

    created tests for utils.py

---
+++ utils_tests.py
@@ -0,0 +1,22 @@
+import utils
+
+utilsObj = utils.utils
+
+print(utilsObj.reversed(123456789))
+print(utilsObj.reversed(123.4569))
+print(utilsObj.reversed("abhay"))
+
+
+print(utilsObj.formatter(123456789))
+print(utilsObj.formatter(2321.2321))
+print(utilsObj.formatter("walia"))
+
+
+print(utilsObj.reversed(9934232))
+print(utilsObj.reversed(456.2567))
+print(utilsObj.reversed("walia"))
+
+
+print(utilsObj.formatter(9934232))
+print(utilsObj.formatter(456.2567))
+print(utilsObj.formatter("abhay"))
